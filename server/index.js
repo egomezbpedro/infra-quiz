829695886306a3000c5aeb821067419162909f81
Pedro Enrique Gómez <57415533+egomezbpedro@users.noreply.github.com>
2023-04-05 10:14:38 +0000

    fixing mongourl to match statefulset

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-const createQuizLoop = require('/api-server/controllers/quizController');
+const createQuizLoop = require('./controllers/quizController');
 
 // Import the application ROUTES
-const userRouter = require('/api-server//routes/userRoute');
-const quizRouter = require('/api-server/routes/quizRoute');
+const userRouter = require('./routes/userRoute');
+const quizRouter = require('./routes/quizRoute');
 
 app.use((req, res, next) => {
   console.log(req.path, req.method, req.body);
